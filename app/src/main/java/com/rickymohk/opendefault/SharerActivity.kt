e567c65e5a947a85e6b85f5dc85cb287bd144352
rickymo <ricky.mo@beluntech.com>
2024-03-25 17:47:21 +0800

    added missing files

---
+++ app/src/main/java/com/rickymohk/opendefault/SharerActivity.kt
@@ -0,0 +1,34 @@
+package com.rickymohk.opendefault
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class SharerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sharer)
+
+        if(intent.action == Intent.ACTION_SEND)
+        {
+            val uri = intent.extras
+                ?.getString(Intent.EXTRA_TEXT)
+                ?.let { Uri.parse(it) }
+            Log.i("OpenDefault","uri: $uri, scheme: ${uri?.scheme}")
+
+            if(uri?.scheme.isNullOrBlank())
+            {
+                Toast.makeText(this, "Invalid URL", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                startActivity(Intent(Intent.ACTION_VIEW, uri))
+            }
+            finish()
+            return
+        }
+    }
+}
